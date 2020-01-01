b9c5d696788911e0676acd70752aa43d10347401
ozzafar <ozzafar@mail.tau.ac.il>
2020-01-01 02:10:15 +0200

    score is calculated by combination of likes and dislikes amounts like/dislike colors

--- src/app/pages/point-of-review/score/score.component.ts
+++ src/app/pages/point-of-review/score/score.component.ts
@@ -5,6 +5,7 @@ import {CodeSnippetsData} from '../../../@core/data/code-snippets';
 import {CodeSnippet} from '../../../@core/lib/objects/code-snippet';
 import {Impression} from '../../../@core/lib/objects/impression';
 import {ImpressionRequest} from '../../../@core/lib/objects/impression-request';
+import {equal} from "assert";
 
 @Component({
   selector: 'ngx-score',
@@ -14,6 +15,8 @@ import {ImpressionRequest} from '../../../@core/lib/objects/impression-request';
 export class ScoreComponent implements OnInit {
   @Input() score: Score;
   @Input() snippet: CodeSnippet;
+  toggle1 = false;
+  toggle2 = false;
   constructor(private codeSnippetsService: CodeSnippetsData, iconsLibrary: NbIconLibraries) {
     iconsLibrary.registerFontPack('fa', { packClass: 'fa', iconClassPrefix: 'fa' });
     iconsLibrary.registerFontPack('fas', { packClass: 'fas', iconClassPrefix: 'fa' });
@@ -21,6 +24,18 @@ export class ScoreComponent implements OnInit {
   }
   vote(voterId: String, impression: Impression): void {
     console.log('voting');
+    if (impression == 0) {
+      this.toggle1 = !this.toggle1;
+      if (this.toggle2 == true){
+        this.toggle2 = false;
+      }
+    }
+    if (impression == 1) {
+      this.toggle2 = !this.toggle2;
+      if (this.toggle1 == true){
+        this.toggle1 = false;
+      }
+    }
     this.codeSnippetsService.updateSnippetImpressions(
       new ImpressionRequest(this.snippet, voterId, impression)).subscribe(
         (score) => {this.score = score; });
@@ -31,6 +46,16 @@ export class ScoreComponent implements OnInit {
       } else {
       return this.score.impressions[impression];
       }
+  }
+  getScore(): number {
+    if (this.score.voterToImpression == null || this.score.voterToImpression.size == 0) {
+      return 0;
+    }
+
+    const likesPowered = this.getImpression('LIKE');
+    const dislikes = this.getImpression('DISLIKE');
+
+    return 100 * (likesPowered / (likesPowered + dislikes));
   }
   ngOnInit() {
   }
